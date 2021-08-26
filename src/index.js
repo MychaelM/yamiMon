7a4b079ac2889ecd68951f0f1fa12055dda78640
Mychael M <mychaelbmenges@gmail.com>
2021-08-26 17:06:57 -0600

    initial repo cleanup

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  useHistory,
+  Switch,
+} from 'react-router-dom';
+import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
+
+
+import { NotFoundPage } from './components/pages/NotFound';
+import { ExampleListPage } from './components/pages/ExampleList';
+import { LoginPage } from './components/pages/Login';
+import { LandingPage } from './components/pages/Landing';
+import { config } from './utils/oktaConfig';
+
+ReactDOM.render(
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
+);
+
+function App() {
+  // The reason to declare App this way is so that we can use any helper functions we'd need for business logic, in our case auth.
+  // React Router has a nifty useHistory hook we can use at this level to ensure we have security around our routes.
+  const history = useHistory();
+
+  const authHandler = () => {
+    // We pass this to our <Security /> component that wraps our routes.
+    // It'll automatically check if userToken is available and push back to login if not :)
+    history.push('/login');
+  };
+
+  return (
+    <Security {...config} onAuthRequired={authHandler}>
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/implicit/callback" component={LoginCallback} />
+        <Route path="/landing" component={LandingPage} />
+        {/* any of the routes you need secured should be registered as SecureRoutes */}
+        <Route
+          path="/"
+          exact
+          component={() => <LandingPage />}
+        />
+        <Route path="/example-list" component={ExampleListPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Security>
+  );
+}
