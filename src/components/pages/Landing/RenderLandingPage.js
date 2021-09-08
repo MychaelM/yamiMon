559258389c9be85a0c6fcbf7f7e3d65ad6e633db
Mychael M <mychaelbmenges@gmail.com>
2021-09-08 17:01:06 -0600

    updated copy on landing page component to better match project

--- src/components/pages/Landing/RenderLandingPage.js
+++ src/components/pages/Landing/RenderLandingPage.js
@@ -4,14 +4,11 @@ import { Link } from 'react-router-dom';
 function RenderLandingPage(props) {
   return (
     <div>
-      <h1>Welcome to Labs Basic SPA</h1>
+      <h1>Landing Page for Yami Collections</h1>
       <div>
+        <p>Placeholder data for landing page information.</p>
         <p>
-          This is an example of how we'd like for you to approach page/routable
-          components.
-        </p>
-        <p>
-          <Link to="/example-list">Example List of Items</Link>
+          <Link to="/">Will link to CTA pages as neccessary</Link>
         </p>
       </div>
     </div>
